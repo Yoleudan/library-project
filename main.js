1af6a54c425062c56501b3c7c1965d8e0a829fa8
yoleudan <158090797+Yoleudan@users.noreply.github.com>
2024-04-14 16:01:51 +0100

    Add read buttons to mark if book has been read or not

--- main.js
+++ main.js
@@ -32,8 +32,8 @@ function getBookInfo(){
     pages = document.getElementById('pageCount').value;
 }
 
-function test(){
-    console.log(library[0]);
+function showLibrary(){
+    console.log(library[0].title)
 }
 
 let openBtn = document.querySelector('#addBookBtn')
@@ -43,28 +43,55 @@ let submit = document.querySelector('#registerBook')
 submit.addEventListener("click", closeForm);
 submit.addEventListener("click", getBookInfo);
 submit.addEventListener("click", addBookToLibrary);
-submit.addEventListener("click", test);
 submit.addEventListener("click", addNewRow);
+submit.addEventListener("click", showLibrary);
+submit.addEventListener("click", addToI);
 
 let table = document.querySelector('#libraryTable')
+
+function toggleRead(){
+    button = document.querySelector('#' + this.id)
+    if(button.style.backgroundColor == "red"){
+        button.style.color = "white"
+        button.style.backgroundColor = "green";
+    }
+    else if(button.style.backgroundColor == "green"){
+        button.style.backgroundColor = "red";
+    }
+}
+
+let i = 0;
+
+function addToI(){
+    return i++
+}
 
 function addNewRow(){
     if ( bookTitle || bookAuthor || pages !== ""){
         let newRow = table.insertRow(-1);
         let newCell1 = newRow.insertCell(0);
-        let newText1 = document.createTextNode(bookTitle);
+        let newText1 = document.createTextNode(library[i].title);
         newCell1.appendChild(newText1);
         let newCell2 = newRow.insertCell(1);
-        let newText2 = document.createTextNode(bookAuthor);
+        let newText2 = document.createTextNode(library[i].author);
         newCell2.appendChild(newText2);
         let newCell3 = newRow.insertCell(2);
-        let newText3 = document.createTextNode(pages);
+        let newText3 = document.createTextNode(library[i].pageCount);
         newCell3.appendChild(newText3);
+        let newCell4 = newRow.insertCell(3);
+        let newRead = document.createElement("button");
+        newRead.style.backgroundColor = "red";
+        newRead.style.color = "white"
+        newRead.textContent = "Read"
+        newRead.id = "read" + i
+        newRead.addEventListener("click", toggleRead)
+        newCell4.appendChild(newRead);
     }
     else{
         alert("Please fill in all fields!")
         openForm();
     }
 }
+
 
 

58fb2bea08c4f1a7811c3994c6778bba68d102ee
yoleudan <158090797+Yoleudan@users.noreply.github.com>
2024-04-14 14:48:35 +0100

    Add function to make new rows with the cell data coming from user input

--- main.js
+++ main.js
@@ -49,11 +49,20 @@ submit.addEventListener("click", addNewRow);
 let table = document.querySelector('#libraryTable')
 
 function addNewRow(){
-    if (bookTitle || bookAuthor || pages !== ""){
-        table.insertRow();
+    if ( bookTitle || bookAuthor || pages !== ""){
+        let newRow = table.insertRow(-1);
+        let newCell1 = newRow.insertCell(0);
+        let newText1 = document.createTextNode(bookTitle);
+        newCell1.appendChild(newText1);
+        let newCell2 = newRow.insertCell(1);
+        let newText2 = document.createTextNode(bookAuthor);
+        newCell2.appendChild(newText2);
+        let newCell3 = newRow.insertCell(2);
+        let newText3 = document.createTextNode(pages);
+        newCell3.appendChild(newText3);
     }
     else{
-        alert("Please fill in form parameters")
+        alert("Please fill in all fields!")
         openForm();
     }
 }
